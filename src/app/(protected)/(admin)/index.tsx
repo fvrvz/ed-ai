b280e1855ccff352a3b36bfbf3465f10e0b80bb1
Faraaz <codefaraaz@gmail.com>
2026-05-25 20:46:43 +0530

    feat: add loading state to KPIs in admin dashboard

--- src/app/(protected)/(admin)/index.tsx
+++ src/app/(protected)/(admin)/index.tsx
@@ -4,7 +4,7 @@ import { useAuth } from "@/hooks/useAuth";
 import { globalStyles } from "@/styles/global";
 import { getCourses, getDocuments, getUsers } from "@/utils/db";
 import { useEffect, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function HomeScreen() {
   const { profile } = useAuth();
@@ -12,6 +12,7 @@ export default function HomeScreen() {
   const [totalUsers, setTotalUsers] = useState(0);
   const [totalCourses, setTotalCourses] = useState(0);
   const [totalDocuments, setTotalDocuments] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   async function fetchKPIs() {
     try {
@@ -26,6 +27,8 @@ export default function HomeScreen() {
       setTotalDocuments(documentsRes.length);
     } catch (error) {
       console.error("Error fetching KPIs:", error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -37,9 +40,15 @@ export default function HomeScreen() {
     <ScrollView style={globalStyles.container}>
       <Title>Welcome {profile?.first_name || "User"}</Title>
       <View>
-        <KPICard title="Total Active Users" value={totalUsers} />
-        <KPICard title="Total Published Courses" value={totalCourses} />
-        <KPICard title="Total Documents" value={totalDocuments} />
+        {loading ? (
+          <Text>Loading KPIs...</Text>
+        ) : (
+          <>
+            <KPICard title="Total Active Users" value={totalUsers} />
+            <KPICard title="Total Published Courses" value={totalCourses} />
+            <KPICard title="Total Documents" value={totalDocuments} />
+          </>
+        )}
       </View>
     </ScrollView>
   );
