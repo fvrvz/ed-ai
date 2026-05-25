75bd83d13b53d2ab812680c93aef443ff2cdd07a
Faraaz <codefaraaz@gmail.com>
2026-05-25 18:57:33 +0530

    feat: show KPIs in admin dashboard

--- src/app/(protected)/(admin)/index.tsx
+++ src/app/(protected)/(admin)/index.tsx
@@ -2,18 +2,44 @@ import KPICard from "@/components/KPICard";
 import Title from "@/components/Title";
 import { useAuth } from "@/hooks/useAuth";
 import { globalStyles } from "@/styles/global";
+import { getCourses, getDocuments, getUsers } from "@/utils/db";
+import { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
 
 export default function HomeScreen() {
   const { profile } = useAuth();
 
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalCourses, setTotalCourses] = useState(0);
+  const [totalDocuments, setTotalDocuments] = useState(0);
+
+  async function fetchKPIs() {
+    try {
+      const [usersRes, coursesRes, documentsRes] = await Promise.all([
+        getUsers({ isActive: true }),
+        getCourses({ isActive: true }),
+        getDocuments(),
+      ]);
+
+      setTotalUsers(usersRes.length);
+      setTotalCourses(coursesRes.length);
+      setTotalDocuments(documentsRes.length);
+    } catch (error) {
+      console.error("Error fetching KPIs:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchKPIs();
+  }, []);
+
   return (
     <ScrollView style={globalStyles.container}>
       <Title>Welcome {profile?.first_name || "User"}</Title>
       <View>
-        <KPICard title="Total Users" value={1234} />
-        <KPICard title="Active Sessions" value={567} />
-        <KPICard title="Revenue" value="$12,345" />
+        <KPICard title="Total Active Users" value={totalUsers} />
+        <KPICard title="Total Published Courses" value={totalCourses} />
+        <KPICard title="Total Documents" value={totalDocuments} />
       </View>
     </ScrollView>
   );
